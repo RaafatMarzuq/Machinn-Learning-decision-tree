771f0123a4f8bd7216319906a8ee85b59f606ec8
RaafatMarzuq <57416873+RaafatMarzuq@users.noreply.github.com>
2022-01-10 21:02:27 +0200

    building tree

--- DecisionTree.py
+++ DecisionTree.py
@@ -7,4 +7,28 @@ import matplotlib.image as pltimg
 
 df = pandas.read_csv("vectors.txt")
 
-print(df)
+
+class Tree:
+    def __init__(self, val = None):
+            if val != None:
+                self.val = val
+            else:
+                self.val = None
+                
+            self.left = None
+            self.right = None
+
+    def insert(self, val):
+            if self.val:
+                if val < self.val:
+                        if self.left is None:
+                            self.left = Tree(val)
+                        else:
+                            self.left.insert(val)
+                elif val > self.val:
+                        if self.right is None:
+                            self.right = Tree(val)
+                        else:
+                            self.right.insert(val)
+            else:
+                self.val = val
