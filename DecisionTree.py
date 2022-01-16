c3414c94443ef9e83aa0df6148062b977ae2ffa2
RaafatMarzuq <57416873+RaafatMarzuq@users.noreply.github.com>
2022-01-16 21:58:13 +0200

    done

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,30 +1,16 @@
 from asyncio.windows_events import INFINITE
 from errno import ENFILE
-from logging import root
-from random import  randrange
 import math
-from re import I, T
-from turtle import left, right
-# class Node:
-#     def __init__(self,coordinate,tag) :
-#         self.left=None
-#         self.right=None
-#         self.tag=tag
-#         self.isLeaf=True
-#         self.counter=0
-#         self.coordinate = coordinate
-#         self.matchTag=0
-#         self.dontMatchTag=0
+
 
 class Tree:
         def __init__(self,tag):
-            self.name=""
             self.left = None
             self.right= None
             self.tag=tag
             self.isLeaf=True
             self.counter=0
-            self.coordinate = "leaf"
+            self.coordinate = 0
             self.vectors_with_tag_1 = 0 
             self.matchTag=0
             self.vectors_with_tag_0=0
@@ -56,40 +42,39 @@ class Tree:
                     for v in vectors:
                             self.insertQ1A(v , k-1)
                     leaves= self.getLeaves()
-                    err_in_tree =0
-                    for i in leaves:
+                    err_in_tree =self.getError(leaves)
+                
+                    return (err_in_tree/150)
+        def getError(self,leaves):
+            err_in_tree=0
+            for i in leaves:
                         if i.tag ==0 :
                             err_in_tree+= (i.vectors_with_tag_1)
                         elif i.tag == 1:
                             err_in_tree+=(i.vectors_with_tag_0 )
-                
-                    return (err_in_tree/150)
-            
+            return err_in_tree
+
         def insertQ1A(self ,vector,k):
             if k > 0:
                 coordinate= self.coordinate 
                 if vector[coordinate] == 0:
                     if self.left is None:
                         self.left = Tree(0)
-                        self.left.name = "<-- left"
                         self.left.insertQ1A(vector,k-1)
                         k-=1
                     else:
                         self.isLeaf=False
-                        self.left.name = "<-- left"
                         self.left.insertQ1A(vector,k-1)
                 elif vector[coordinate]==1:
                     if self.right is None:
                         self.right=Tree(1)
-                        self.right.name = "right -->"
                         self.right.insertQ1A(vector,k-1)
                         k-=1
                     else:
                         self.isLeaf=False
-                        self.right.name = "right -->"
                         self.right.insertQ1A(vector,k-1)
             if k==0:
-                # print("last level")
+               
                     self.counter+=1
                     if vector[8] ==1:
                                 self.vectors_with_tag_1+=1
@@ -118,8 +103,7 @@ class Tree:
                 for i in range(n):
                     self.left = Tree(0)
                     self.right= Tree(1)
-                    # print("left vectors = {}".format(self.left.vectors))
-                    # print("right vectors = {}".format(self.right.vectors))
+                 
                     for v in vectors: 
                         if v[i] == 0 and v[n]==0:
                             self.left.counter+=1
@@ -140,21 +124,17 @@ class Tree:
                         
                     left_entropy = self.left.getEntropy()
                     right_entropy = self.right.getEntropy()
-                    # print("min binary entropy = ",(left_entropy + right_entropy))
                     if  (left_entropy + right_entropy) < min_B_entropy:
                             
                             min_B_entropy = left_entropy + right_entropy
                             best_i=i
                             left = self.left
                             right=self.right
-                    
-                        
-                    # print("left  = {}".format(left.vectors))
-                    # print("right  = {}".format(right.vectors))
                 self.left=left
                 self.right=right
                 return [best_i, min_B_entropy] 
-                    
+        # the two methods display and display_aux i took them
+        #  from https://stackoverflow.com/questions/34012886/print-binary-tree-level-by-level-in-python and made some changes       
         def display(self):
             lines, *_ = self._display_aux()
             for line in lines:
@@ -217,29 +197,16 @@ class Tree:
             self.left.coordinate= left_split[0]
             self.right.coordinate=right_split[0]
 
-            return [self_split[0],left_split[0],right_split[0]]
+            return [self,self_split[0],left_split[0],right_split[0]]
                 
               
-               
-            
-            
-
-            
-
-            
-            
-            
-
-
-
 def Q1B(vectors,k):
     tree = Tree(-1)
     root_split = tree.insertQ1B(k,vectors)
-    tree.display()
-    return root_split
-
-
-
+    tree= root_split[0]
+    leaves= tree.getLeaves()
+    error_in_tree = tree.getError(leaves)  
+    return [root_split, (error_in_tree/150)]
 
 def Q1A(vectors , k):
     n= len(vectors[0])-1
@@ -251,14 +218,12 @@ def Q1A(vectors , k):
                 tree = Tree(-1)
                 tree.coordinate=i
                 tree.name="Root" 
-                # print("new tree")   
                 tree_error= tree.generateTree(vectors,k,l,r)
                 if tree_error < min_error:
                     min_error= tree_error
                     tree.min_error_coordinates.append("Root cooardinare {}".format(i))
                     tree.min_error_coordinates.append("Left Node cooardinare {}".format(l))
                     tree.min_error_coordinates.append("Right Node cooardinare {}".format(r))
-                    # print("new min error = {:.2f}".format(min_error))
                     
                     best_indexs=[tree,min_error]
                     
@@ -267,28 +232,18 @@ def Q1A(vectors , k):
 
 
 def main(vectors,k):
-        # best_tree= Q1A(vectors,k)
-       
-        best_tree_entropy = Q1B(vectors,k)
-        print("root coordinate = ",best_tree_entropy[0])
-        print("left coordinate = ",best_tree_entropy[1])
-        print("right coordinate = ",best_tree_entropy[2])
-
-        # tree =Tree(-1)
-        # tree =best_tree[0]
-        # tree_error=best_tree[1]   
-        # for i in tree.min_error_coordinates:
-        #     print(i)
-        # print("with error = {:.2f}".format(tree_error))
+        best_tree= Q1A(vectors,k)
+        print("Q 1.A :-")
+        print("The {} levels tree with the lowest error :".format(k))
+        best_tree[0].display()
+        print("With error = " , best_tree[1])
+        print("\nQ 1.B :-")
+        print("The {} levels tree using binary entropy :".format(k))
         
-        # print("The {} levels tree with the lowest error is the one with:".format(k)) 
-        # print("root cooardinate {}".format(best_coordinates[0]))
-        # print("left node coordinate {}".format(best_coordinates[1]))
-        # print("right node coordinate {}".format(best_coordinates[2]))
-        # print("with error = {:.2f}".format(best_coordinates[3]))
-    # tree=Tree(-1)
-    # tree.insertQ1B(3,vectors)
-
+        best_tree_entropy = Q1B(vectors,k)
+        best_tree_entropy[0][0].display()
+        print("with error = {:.2f} ".format(best_tree_entropy[1]))
+      
 if __name__ == '__main__':
    
     f = open("vectors.txt", "r")
