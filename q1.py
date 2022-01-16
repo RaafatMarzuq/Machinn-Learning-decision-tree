62cff857cadc237818a9d3dca6f9fdc1606b4104
AseelNemer <AseelNemer@users.noreply.github.com>
2022-01-16 17:21:57 +0200

    q1
    q1

--- q1.py
+++ q1.py
@@ -1,6 +1,12 @@
+# from asyncio.windows_events import INFINITE
 from itertools import count
-from random import  randrange
+from json.encoder import INFINITY
+# from random import  randrange
 import math
+# from this import d
+# from tkinter.messagebox import NO
+
+
 
 class Node:
     def __init__(self , isleaf = False, tag=1) :
@@ -12,6 +18,8 @@ class Node:
         self.coordinate = 0
         self.matchTag=0
         self.dontMatchTag=0
+        self.minentrop=0
+        self.vector=[]
     
 class Tree:
         def __init__(self,k):
@@ -25,13 +33,9 @@ def Recursion(k, coordinate_num, vectors ,t):
     for i in range(coordinate_num):
         t=Tree(k)
         t.root.coordinate=i
-<<<<<<< HEAD
-
-=======
      
->>>>>>> de09d93080aba09d532b00282f4dcfe29064b81b
         for j in range(coordinate_num): # to get the best left and right node 
-            # if(j!=i):
+            if(True):
                 t.root.left=Node()
                 left=t.root.left
                 count1=0 
@@ -46,6 +50,7 @@ def Recursion(k, coordinate_num, vectors ,t):
                         count1+=1
                     elif(v[i]==0 and v[j]==1 and v[8]==0):
                         count2+=1
+                # p1=count1+count2
                 if( count1>count2 and count1>left.matchTag):
                     left.left=Node(True,0)
                     left.right=Node(True,1)
@@ -89,15 +94,6 @@ def Recursion(k, coordinate_num, vectors ,t):
                     right.coordinate=j
                 # print("right matching = ", right.matchTag)
 
-<<<<<<< HEAD
-                print("coordinate = ", t.root.coordinate)
-                print("t right = ", t.root.right.matchTag)
-                print("t left = ", t.root.left.matchTag)
-                print("t left coord = ", t.root.left.coordinate)
-                print("t right coord = ", t.root.right.coordinate)
-                print("t left counter = ", t.root.left.counter)
-                print("t right counter = ", t.root.right.counter)
-=======
         # print("coordinate = ", t.root.coordinate)
         # print("t right = ", t.root.right.matchTag)
         # print("t left = ", t.root.left.matchTag)
@@ -105,7 +101,6 @@ def Recursion(k, coordinate_num, vectors ,t):
         # print("t right coord = ", t.root.right.coordinate)
         # print("t left counter = ", t.root.left.counter)
         # print("t right counter = ", t.root.right.counter)
->>>>>>> de09d93080aba09d532b00282f4dcfe29064b81b
 
 
 
@@ -122,7 +117,112 @@ def Recursion(k, coordinate_num, vectors ,t):
                 
                    
                             
- 
+def q2(k, coordinate_num, vectors ,t):
+    t1=Tree(k)
+    minentrop=INFINITY
+    for i in range(coordinate_num):
+        count1=0 
+        count2=0 
+        count3=0
+        count4=0
+        for j in vectors:
+            if (j[i]==0 and j[8]==1):
+                count1+=1
+            elif (j[i]==0 and j[8]==0 ):
+                count2 +=1
+            if (j[i]==1 and j[8]==1):
+                count3+=1
+            elif (j[i]==1 and j[8]==0 ):
+                count4 +=1
+        print ("coordinate = ", i," count1= ", count1," count2= ", count2," count3= ", count3," count4= ", count4 )
+
+        p1=0
+        p2=0
+        if(count1!=0 and count2!=0 and count3!=0 and count4!=0):
+            p1=count1/(count1+count2)
+            p2=count3/(count3+count4)
+        print("p1 = ",p1 ," p2 = ",p2)
+        
+        entrop=(-p1*(math.log(1/p1)) - (1-p1)*(math.log(1/(1-p1)))) + (-p2*(math.log(1/p2)) - (1-p2)*(math.log(1/(1-p2))))
+
+        print("entropy = ", entrop)
+
+        if(entrop<minentrop):
+            t1.root.coordinate=i
+            t1.root.minentrop=entrop
+            minentrop=entrop
+    t1.root.left=Node(k-1)
+    t1.root.right=Node(k-1)
+    left=t1.root.left
+    right=t1.root.left
+    t1.root.left = best_entrop(left,0, vectors, coordinate_num)
+    t1.root.right = best_entrop(right,1,vectors, coordinate_num)
+    
+
+    return t1
+
+def best_entrop(t1, num, vectors, coor):
+    t=Node()
+    minentrop=INFINITY
+    for i in range(coor-1):
+        count1=0
+        count2=0
+        count3=0
+        count4=0
+        if(i!=num):
+            for j in vectors:
+           
+                if (j[num]==0 and j[i]==0 and j[8]==1):
+                    count1+=1
+                elif(j[num]==0 and j[i]==0 and j[8]==0):
+                    count2+=1
+                if (j[num]==0 and j[i]==1 and j[8]==1):
+                    count3+=1
+                elif(j[num]==0 and j[i]==1 and j[8]==0):
+                    count4+=1
+    
+            p1=0
+            p2=0
+            if(count1!=0 and count2!=0 and count3!=0 and count4!=0):
+                p1=count1/(count1+count2)
+                p2=count3/(count3+count4)
+
+            entrop=(-p1*(math.log(1/p1)) - (1-p1)*(math.log(1/(1-p1)))) + (-p2*(math.log(1/p2)) - (1-p2)*(math.log(1/(1-p2))))
+            
+            if(entrop<minentrop):
+                minentrop=entrop
+                t.coordinate=i
+                t.minentrop=entrop
+    t1=t       
+    return t1
+
+def r2(k,coordinate, tree , vectors):
+    if k==1:
+       return Tree
+
+    for i in range(coordinate):
+        print(i)
+        count1,count2, count3, count4=0
+        for j in vectors:
+            if j[i]==0 and j[8]==1:
+                count1+=1
+            if j[i]==0 and j[8]==0:
+                count2+=1
+            if j[i]==1 and j[8]==0:
+                count3+=1
+            if j[i]==1 and j[8]==1:
+                count4+=1 
+
+        p1,p2=0
+        if(count1!=0 and count2!=0 and count3!=0 and count4!=0):
+            p1=count1/(count1+count2)
+            p2=count3/(count3+count4)
+        entropy=(-p1*(math.log(1/p1)) - (1-p1)*(math.log(1/(1-p1)))) + (-p2*(math.log(1/p1)) - (1-p2)*(math.log(1/(1-p2))))
+
+        
+
+        
+
   
 
 
@@ -136,22 +236,22 @@ if __name__ == '__main__':
         # print(single_vector[8])
         all_vectors.append(single_vector)
     t=Tree(3)
-
-    t=Recursion(3,8,all_vectors ,t)
-    # print ("coordinate",t.root.coordinate)
-    # print ("coordinate",t.root.right.coordinate)
-    # print ("coordinate",t.root.left.coordinate)
-    count1=0
-    count2=0
-    for i in all_vectors:
-        if(i[5]==0 and i[7]==0 and i[8]==1):
-            count1+=1
-        if(i[5]==0 and i[7]==1 and i[8]==0):
-            count1+=1
-        if(i[5]==1 and i[7]==0 and i[8]==0):
-            count2+=1
-        if(i[5]==1 and i[7]==1 and i[8]==1):
-            count2+=1
-    print(count1)
-    print(count2)
+    t=q2(3, 8, all_vectors ,t)
+    # t=Recursion(3,8,all_vectors ,t)
+    print ("coordinate",t.root.coordinate)
+    print ("coordinate",t.root.right.coordinate)
+    print ("coordinate",t.root.left.coordinate)
+    # count1=0
+    # count2=0
+    # for i in all_vectors:
+    #     if(i[1]==0 and i[6]==0 and i[8]==1):
+    #         count1+=1
+    #     if(i[1]==0 and i[6]==1 and i[8]==0):
+    #         count2+=1
+    #     if(i[1]==1 and i[3]==0 and i[8]==0):
+    #         count2+=1
+    #     if(i[1]==1 and i[3]==1 and i[8]==1):
+    #         count2+=1
+    # print(count1)
+    # print(count2)
         
